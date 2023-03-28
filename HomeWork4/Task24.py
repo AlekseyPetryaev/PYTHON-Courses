1d7cc2691ab01f40793087d1ceec90d5c0f312aa
Aleksei <petraevaleksej7@gmail.com>
2023-03-28 09:49:51 +0300

    add Home Work 4

--- HomeWork4/Task24.py
+++ HomeWork4/Task24.py
@@ -14,18 +14,34 @@
 # 4 -> 1 2 3 4
 # 9
 
-n = int(input('Введите кол-во кустов: '))
-a = list(map(int, input().split()))
+# n = int(input('Введите кол-во кустов: '))
+# a = list(map(int, input().split()))
 
-maxsum = 0
+# maxsum = 0
 
-for i in range(n):
-	cursum = sum(a[i:i+3])
-	if cursum > maxsum:
-		maxsum = cursum
-if a[0] + a[-1] + a[-2] > maxsum:
-	maxsum = a[0] + a[-1] + a[-2]
-if a[0] + a[1] + a[-1] > maxsum:
-	maxsum = a[0] + a[1] + a[-1]
+# for i in range(n):
+# 	cursum = sum(a[i:i+3])
+# 	if cursum > maxsum:
+# 		maxsum = cursum
+# if a[0] + a[-1] + a[-2] > maxsum:
+# 	maxsum = a[0] + a[-1] + a[-2]
+# if a[0] + a[1] + a[-1] > maxsum:
+# 	maxsum = a[0] + a[1] + a[-1]
 
-print(f'Всего {maxsum} ягод за один заход')
+# print(f'Всего {maxsum} ягод за один заход')
+
+
+####
+# Эталонное решения
+
+n = int(input())
+arr = list()
+for i in renge(n):
+    x = int(input())
+    arr.append(x)
+    
+arr_count = list()
+for i in range(len(arr) -1):
+    arr_count.append(arr[i -1] + arr[i] +arr[i + 1])
+arr_count.append(arr[-2] + arr[-1] + arr[0])
+print(max(arr_count))
